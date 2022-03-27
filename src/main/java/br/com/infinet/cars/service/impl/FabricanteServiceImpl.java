7a9657255534c4c9d57f195ab388255c8991fd97
leonardogloria <contato@leonardogloria.com.br>
2022-03-27 17:37:56 -0300

    ADicionando chamada de fabricante com restTemplate

--- src/main/java/br/com/infinet/cars/service/impl/FabricanteServiceImpl.java
+++ src/main/java/br/com/infinet/cars/service/impl/FabricanteServiceImpl.java
@@ -4,6 +4,7 @@ import br.com.infinet.cars.model.Fabricante;
 import br.com.infinet.cars.service.FabricanteService;
 import com.google.gson.Gson;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URI;
@@ -15,15 +16,10 @@ import java.net.http.HttpResponse;
 public class FabricanteServiceImpl implements FabricanteService {
     @Override
     public Fabricante getByNome(String nome) throws IOException, InterruptedException {
-        Gson gson = new Gson();
-
         String url = String.format("http://localhost:8081/fabricante/nome/%s", nome);
-        HttpRequest request = HttpRequest.newBuilder(URI.create(url))
-                .GET()
-                .build();
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        Fabricante fabricante = gson.fromJson(response.body(), Fabricante.class);
+        RestTemplate restTemplate = new RestTemplate();
+        Fabricante fabricante = restTemplate.getForObject(url, Fabricante.class);
+
         return fabricante;
 
     }
