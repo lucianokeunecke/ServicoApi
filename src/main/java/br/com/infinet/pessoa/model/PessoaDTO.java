9027b47094395785e9e35cbe1874919209c746b7
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-04-02 10:33:27 -0300

    Trabalho da disciplina de Integração Contínua e DevOps

---
+++ src/main/java/br/com/infinet/pessoa/model/PessoaDTO.java
@@ -0,0 +1,14 @@
+package br.com.infinet.pessoa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PessoaDTO {
+    Pessoa pessoa;
+}
