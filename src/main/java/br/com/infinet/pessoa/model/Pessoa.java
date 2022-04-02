32a7fc1ce0ef88172379e8e728b4c5653cf5b9c0
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-04-02 10:27:56 -0300

    Trabalho da disciplina de Integração Contínua e DevOps

---
+++ src/main/java/br/com/infinet/pessoa/model/Pessoa.java
@@ -0,0 +1,31 @@
+package br.com.infinet.pessoa.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class Pessoa implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cnpjCpf;
+    private String nome;
+    private String endereco;
+    private Long numeroEndereco;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String estado;
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,29 @@
-insert into carro (id, fabricante, modelo, velocidade_maxima) values ('PORCHE','CAYENNE', 223);
-insert into carro (id, fabricante, modelo, velocidade_maxima) values ('PORCHE','BOXTER', 255);
-insert into carro (id, fabricante, modelo, velocidade_maxima) values ('PORCHE','MACAN', 199);
-insert into carro (id, fabricante, modelo, velocidade_maxima) values ('BUGATTI','CHIRON', 401);
-insert into carro (id, fabricante, modelo, velocidade_maxima) values ('MERCEDES','GLA200', 212);
-insert into carro (id, fabricante, modelo, velocidade_maxima) values ('LAMBORGHINI','AVENTADOR', 399);
+-- Table: postgres.cliente
+
+-- DROP TABLE IF EXISTS postgres.cliente;
+
+CREATE TABLE IF NOT EXISTS postgres.cliente
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    cnpj_cpf text COLLATE pg_catalog."default",
+    nome text COLLATE pg_catalog."default",
+    endereco text COLLATE pg_catalog."default",
+    numero_endereco integer,
+    bairro text COLLATE pg_catalog."default",
+    cep text COLLATE pg_catalog."default",
+    cidade text COLLATE pg_catalog."default",
+    estado text COLLATE pg_catalog."default",
+    CONSTRAINT pessoa_pkey PRIMARY KEY (id)
+    )
+
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS postgres.cliente
+    OWNER to postgres;
+
+
+
+insert into cliente (cnpj_cpf, nome, endereco, numero_endereco, bairro, cep, cidade, estado) values ('031.865.695-92', 'Luciano Keunecke','Rua Max Weise', 290, 'Agua Verde', '89.032-280', 'Blumenau', 'SC');
+insert into cliente (cnpj_cpf, nome, endereco, numero_endereco, bairro, cep, cidade, estado) values ('04.965.365/0001-63', 'Eletrônica Blumenau','Rua XV de Novembro', 6856, 'Centro', '89.133-270', 'Blumenau', 'SC');
+insert into cliente (cnpj_cpf, nome, endereco, numero_endereco, bairro, cep, cidade, estado) values ('085.463.695-92', 'Adriano dos Santos','Rua Jaragua', 756, 'Vila Nova', '86.654-145', 'Florianópolis', 'SC');
+insert into cliente (cnpj_cpf, nome, endereco, numero_endereco, bairro, cep, cidade, estado) values ('653.765.413-33', 'Paulo Vieira Souza','Rua das Palmeiras', 98, 'Bela Vista', '86.463-746', 'Timbo', 'SC');
