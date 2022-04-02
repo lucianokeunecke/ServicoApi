6dc0f8424cbd03d7c0785c5cf39b2693cb0e48f6
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-04-02 11:41:31 -0300

    Trabalho da disciplina de Integração Contínua e DevOps

---
+++ src/main/java/br/com/infinet/pessoa/service/impl/CidadeServiceImpl.java
@@ -0,0 +1,12 @@
+package br.com.infinet.pessoa.service.impl;
+
+import br.com.infinet.pessoa.model.Cidade;
+import br.com.infinet.pessoa.service.CidadeService;
+
+public class CidadeServiceImpl implements CidadeService {
+
+    @Override
+    public Cidade getByNome(String nome) {
+        return null;
+    }
+}
