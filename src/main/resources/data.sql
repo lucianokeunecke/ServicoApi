c63adcdd77732149d17d01642548bec427c83084
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-04-02 11:31:33 -0300

    Trabalho da disciplina de Integração Contínua e DevOps

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,21 @@
+-- Table: postgres.cidade
+
+-- DROP TABLE IF EXISTS postgres.cidade;
+
+CREATE TABLE IF NOT EXISTS postgres.cidade
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    nome text COLLATE pg_catalog."default",
+    CONSTRAINT cidade_pkey PRIMARY KEY (id)
+    )
+
+    TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS postgres.cidade
+    OWNER to postgres;
+
+
+
 -- Table: postgres.cliente
 
 -- DROP TABLE IF EXISTS postgres.cliente;
@@ -22,8 +40,11 @@ ALTER TABLE IF EXISTS postgres.cliente
     OWNER to postgres;
 
 
+insert into cidade (nome) values ('Blumenau');
+insert into cidade (nome) values ('Timbo');
+insert into cidade (nome) values ('Florianopolis');
 
 insert into cliente (cnpj_cpf, nome, endereco, numero_endereco, bairro, cep, cidade, estado) values ('031.865.695-92', 'Luciano Keunecke','Rua Max Weise', 290, 'Agua Verde', '89.032-280', 'Blumenau', 'SC');
 insert into cliente (cnpj_cpf, nome, endereco, numero_endereco, bairro, cep, cidade, estado) values ('04.965.365/0001-63', 'Eletrônica Blumenau','Rua XV de Novembro', 6856, 'Centro', '89.133-270', 'Blumenau', 'SC');
-insert into cliente (cnpj_cpf, nome, endereco, numero_endereco, bairro, cep, cidade, estado) values ('085.463.695-92', 'Adriano dos Santos','Rua Jaragua', 756, 'Vila Nova', '86.654-145', 'Florianópolis', 'SC');
+insert into cliente (cnpj_cpf, nome, endereco, numero_endereco, bairro, cep, cidade, estado) values ('085.463.695-92', 'Adriano dos Santos','Rua Jaragua', 756, 'Vila Nova', '86.654-145', 'Florianopolis', 'SC');
 insert into cliente (cnpj_cpf, nome, endereco, numero_endereco, bairro, cep, cidade, estado) values ('653.765.413-33', 'Paulo Vieira Souza','Rua das Palmeiras', 98, 'Bela Vista', '86.463-746', 'Timbo', 'SC');
