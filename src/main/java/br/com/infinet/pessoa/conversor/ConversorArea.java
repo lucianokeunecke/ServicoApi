c361dfdd84013dc7d7eb0ee55337181a64f1c870
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-04-09 10:23:48 -0300

    Trabalho da disciplina Integração Continua e DevOps

---
+++ src/main/java/br/com/infinet/pessoa/conversor/ConversorArea.java
@@ -0,0 +1,29 @@
+package br.com.infinet.pessoa.conversor;
+
+public class ConversorArea {
+
+    public double HectareParaMetroQuadrado(double hectare) {
+        return hectare * 10000;
+    }
+
+    public double HectareParaQuilometroQuadrado(double hectare) {
+        return hectare / 100;
+    }
+
+    public double MetroQuadradoParaHectare(double metroQuadrado) {
+        return metroQuadrado / 10000;
+    }
+
+    public double MetroQuadradoParaQuilometroQuadrado(double metroQuadrado) {
+        return metroQuadrado / 1e+6;
+    }
+
+    public double QuilometroQuadradoParaHectare(double quilometroQuadrado) {
+        return quilometroQuadrado * 100;
+    }
+
+    public double QuilometroQuadradoParaMetroQuadrado(double quilometroQuadrado) {
+        return quilometroQuadrado * 1e+6;
+    }
+
+}
