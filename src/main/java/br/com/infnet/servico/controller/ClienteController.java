b6cd47caa0ad013e3e65819433658efb471ac32d
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-06-16 19:02:51 -0300

    Removendo a classe geraExcecao

--- src/main/java/br/com/infnet/servico/controller/ClienteController.java
+++ src/main/java/br/com/infnet/servico/controller/ClienteController.java
@@ -17,13 +17,8 @@ public class ClienteController {
     @Autowired
     ClienteService clienteService;
 
-    @Autowired
-    GerarExcecaoAleatoriaService gerarExcecaoAleatoriaService;
-
     @GetMapping
     public ResponseEntity<List<Cliente>> listarTodos(){
-
-        gerarExcecaoAleatoriaService.gerar();
 
         List<Cliente> all = clienteService.buscarTodos();
 
@@ -32,8 +27,6 @@ public class ClienteController {
 
     @GetMapping("/{id}")
     public ResponseEntity< ? > listarPeloId(@PathVariable Long id){
-
-        gerarExcecaoAleatoriaService.gerar();
 
         Optional<Cliente> cliente = clienteService.buscarPeloId(id);
 
@@ -46,23 +39,17 @@ public class ClienteController {
     @PostMapping
     public void incluir(@RequestBody Cliente cliente){
 
-        gerarExcecaoAleatoriaService.gerar();
-
         clienteService.incluir(cliente);
     }
 
     @PutMapping("/{id}")
     public void alterar(@PathVariable  Long id, @RequestBody Cliente cliente){
 
-        gerarExcecaoAleatoriaService.gerar();
-
         clienteService.alterar(id, cliente);
     }
 
     @DeleteMapping("/{id}")
     public void excluir(@PathVariable  Long id){
-
-        gerarExcecaoAleatoriaService.gerar();
 
         clienteService.excluir(id);
     }
