4756d38738785d0646e7351ad6a4deed008212a1
EduardoCarriel <eduardo_carriel@live.com>
2022-06-19 09:27:05 -0300

    Trabalho da disciplina Clean Code e Padrões de Projeto

--- src/main/java/br/com/infnet/servico/validation/impl/CnpjPessoaValidationImpl.java
+++ src/main/java/br/com/infnet/servico/validation/impl/CnpjPessoaValidationImpl.java
@@ -6,11 +6,10 @@ import br.com.infnet.servico.validation.PessoaValidation;
 
 public class CnpjPessoaValidationImpl implements PessoaValidation {
     @Override
-    public void validar(Pessoa fornecedor) {
-        if (fornecedor.getTipoPessoa().getId() == EnumTipoPessoa.PESSOA_JURIFICA.getValor()) {
-            if (fornecedor.getCnpjCpf().length() != 18) {
-                throw new RuntimeException("CNPJ inválido.");
-            }
+    public void validar(Pessoa pessoa) {
+        if (!pessoa.EhPessoaJuridica()) return;
+        if (pessoa.getCnpjCpf().length() != 18) {
+            throw new RuntimeException("CNPJ inválido.");
         }
     }
 }
